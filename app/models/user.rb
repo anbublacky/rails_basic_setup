cb3d57158b0ec0f2f3812bcbacea68d0d1ca7c52
Anbazhagan <anbublacky@gmail.com>
2015-07-18 13:39:01 +0530

    changes to facebook signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,18 @@ class User < ActiveRecord::Base
 		Rails.logger.info auth.provider  
 		Rails.logger.info auth.uid 
 		Rails.logger.info auth.info.email
-	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-	    user.email = auth.info.email
-	    user.password = Devise.friendly_token[0,20]
-	    # user.name = auth.info.name   # assuming the user model has a name
-	    # user.image = auth.info.image # assuming the user model has an image
-	  end
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    if user
+       return user
+    else
+      user = User.new(provider:auth.provider,
+                      uid:auth.uid,
+                      email:auth.info.email,
+                      password:Devise.friendly_token[0,20])
+      user.skip_confirmation!
+      user.save
+      return user
+		end
 	end
 
   def self.new_with_session(params, session)
